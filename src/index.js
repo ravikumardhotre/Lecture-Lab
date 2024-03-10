0f111695c059cf4e257dba509fbf9edfa7a6df60
Ravikumar Dhotre <93571776+ravikumardhotre@users.noreply.github.com>
2024-03-10 16:46:58 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 var bodyParser = require("body-parser");
-
+const bcrypt = require("bcrypt");
 const route = require("./routes/route.js");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const PORT = 3000;
-// const IP_ADDRESS = "localhost";
+const session = require("express-session");
+const passport = require("passport");
+const Oauth2Strategy = require("passport-google-oauth20").Strategy;
+const IP_ADDRESS = "localhost";
 const path = require("path");
 
 const corsOptions = {
@@ -46,6 +49,59 @@ cloudinary.config({
   api_secret: process.env.API_SECRET,
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+
+
+const Instructor = require("./Model/instructorModel.js");
+
+ 
+app.use(session({ secret: process.env.SESSION_SECRET, resave: true, saveUninitialized: true }));
+
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new Oauth2Strategy({
+
+  clientID: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+  callbackURL: process.env.CALLBACK_URL,
+  scope: ["email", "profile"],
+
+},async function (accessToken, refreshToken, profile, done) {
+  console.log(profile,accessToken,refreshToken);
+  try{
+    let userEmail = profile.emails[0].value;
+    // Modify email domain if it's gmail.com
+    userEmail = userEmail.replace('@gmail.com', '@lectureLab.com');
+
+    let user = await Instructor.findOne({ email: userEmail });;
+    if (user) {
+      return done(null, user);
+    } else {
+      // Hash the password
+ const hashedPassword = await bcrypt.hash("Pass@123", 10);
+      let user = await Instructor.create({ name: profile.displayName, email:userEmail,mobile:profile._json.mobile || "1234567890", password:hashedPassword, role: "user", isDeleted: false, isActive: true, createdAt: Date.now() });
+      return done(null, user);
+    }
+  } catch (error) {
+    return done(error);
+}
+  
+}));
+
+
+
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+
+
+passport.deserializeUser(function (user, done) {
+  done(null, user);
+});
+
+app.get("auth/google", passport.authenticate("google", { scope: ["profile", "email"] }));
+app.get("auth/google/callback", passport.authenticate("google", { failureRedirect: "https://lecture-lab.web.app/login", successRedirect: "https://lecture-lab.web.app/assigned-batches" }));
+
+app.listen(PORT, IP_ADDRESS, () => {
+  console.log(`Server is running on http://${IP_ADDRESS}:${PORT}`);
 });
